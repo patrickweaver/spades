ae0ccb064f3caa2d677179ab8312fef27c36a0d1
Patrick Weaver <pjpwea@gmail.com>
2017-11-12 17:35:32 -0500

    web requests working with neural network

--- classes/player.js
+++ classes/player.js
@@ -37,7 +37,7 @@ class Player {
     this.stage = stage;
     this.prompt = prompt;
   }
-  
+
   setTeamName(game, teamNameChoice){
     this.teamNameChoice = teamNameChoice;
     var allTeamNamesChosen = true;
@@ -57,7 +57,7 @@ class Player {
       game.newHand();
     }
   }
-  
+
 
   getBid(hand) {
     this.stage = "bidNow";
@@ -75,7 +75,7 @@ class Player {
       });
     }
   }
-  
+
   //✌️ But this time it's the whole player not just the id in game.bidorder
   findSelfInOrder(playerId, order) {
     if (order && order.length === 4) {
@@ -88,7 +88,7 @@ class Player {
       return 0;
     }
   }
-  
+
   //✌️
   findSelfInTeam(playerId, teamInfo) {
     if (teamInfo && teamInfo.length === 2) {
@@ -103,7 +103,7 @@ class Player {
       return [0, 0];
     }
   }
-  
+
   cardValues() {
     var handValues = [];
     for (var c in this.handCards) {
@@ -115,17 +115,18 @@ class Player {
     }
     return handValues;
   }
-  
+
   collectBotInfo(infoType, hand) {
     // infoType should either be "bid" or "play"
-    
+
     var strategy;
     if (infoType === "bid") {
-      strategy = "numberOfSpades";
+      strategy = "nn";
+      //strategy = "numberOfSpades";
     } else if (infoType === "play") {
       strategy = "random";
-    }    
-    
+    }
+
     var game = hand.game;
     var handNumber = game.hands.length;
     var selfInBidOrder = this.findSelfInOrder(this.playerId, game.bidOrder);
@@ -137,10 +138,10 @@ class Player {
     var left = game.bidOrder[(selfInBidOrder + 1) % 4];
     var right = game.bidOrder[(selfInBidOrder + 3) % 4];
     var handValues = this.cardValues()
-    
-    
+
+
     var postData = {
-      strategy: strategy,  
+      strategy: strategy,
       gameId: this.gameId,
       handNumber: handNumber,
       playerId: this.playerId,
@@ -155,14 +156,14 @@ class Player {
       scoreOtherTeamScore: otherTeam.score,
       scoreOtherTeamBags: otherTeam.bags
     }
-    
+
     if (infoType === "play") {
       var trick = hand.currentTrick();
-      
+
       postData.spadesBroken = trick.spadesBroken;
       postData.trickNumber = hand.tricks.length;
-      
-      
+
+
       function getIfCard(playOrder) {
         if (playOrder < trick.cardsPlayed.length) {
           return trick.cardsPlayed[playOrder].fullValue;
@@ -170,32 +171,32 @@ class Player {
           return false;
         }
       }
-      
-      
+
+
       var selfInPlayOrder = this.findSelfInOrder(this.playerId, trick.playOrder);
       postData.playSelfOrder = selfInPlayOrder + 1;
       postData.playSelfPlay = getIfCard(selfInPlayOrder);
-      
+
       var partnerPlayOrder = (selfInPlayOrder + 2) % 4;
       postData.playPartnerPlay = getIfCard(partnerPlayOrder);
-      
+
       var leftPlayOrder = (selfInPlayOrder + 1) % 4;
       postData.playLeftPlay = getIfCard(leftPlayOrder);
-      
+
       var rightPlayOrder = (selfInPlayOrder + 3) % 4;
-      postData.playRightPlay = getIfCard(rightPlayOrder);      
+      postData.playRightPlay = getIfCard(rightPlayOrder);
     }
-    
+
     return postData;
   }
-  
-  
-  
-  
+
+
+
+
   botBid(hand){
 
     var postData = this.collectBotInfo("bid", hand);
-    
+
     function callback(hand, error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
@@ -211,7 +212,7 @@ class Player {
             for (var k in error[i][j]);
             console.log(i + " ^ " + j + " * " + k + ":  " + error[i][j][k]);
           }
-          
+
         }
       }
     }
@@ -226,7 +227,7 @@ class Player {
     }
     //console.log(this.name + " bids: " + this.bid);
   }
-  
+
   getPlay(trick) {
     if (this.attempts < 13) {
       this.stage = "playNow";
@@ -245,13 +246,13 @@ class Player {
             "question": "It's your turn!",
             "type": "cards",
             "options": []
-          });   
+          });
         } else {
           this.setStatus("playNow", {
             "question": "Invalid card, it's still your turn!",
             "type": "cards",
             "options": []
-          }); 
+          });
         }
       }
     } else {
@@ -263,11 +264,11 @@ class Player {
       });
     }
   }
-  
+
   botPlay(trick) {
     var postData = this.collectBotInfo("play", trick.hand);
-    
-    
+
+
     function callback(error, response, body) {
       if (!error && response.statusCode == 200) {
         var info = JSON.parse(body);
@@ -277,12 +278,12 @@ class Player {
         console.log("ERROR " + error);
       }
     }
-    
+
     helpers.sendToBot("play", postData, callback.bind(this));
   }
-  
+
   playCard(cardIndex, trick) {
-    var card = (this.handCards[cardIndex]);   
+    var card = (this.handCards[cardIndex]);
     if (this.isLegalCard(trick, this.handCards, card)) {
       //console.log("🎴 " + this.name + " plays " + card.fullName);
       if (card.suit === "♠︎") {
@@ -299,30 +300,30 @@ class Player {
       setTimeout(function() {
         this.checkConfirm(trick.hand.game);
         trick.nextPlayer(i + 1);
-        
+
       }.bind(this), 0);
     } else {
       console.log("⛔️ Illegal Card "  + this.attempts);
       this.illegalCardReset(trick);
     }
   }
-  
+
   confirmPlay(game){
     this.confirmed = true;
     this.checkConfirm(game);
   }
-  
+
   checkConfirm(game) {
     if (this.type === "bot") {
       this.confirmed = true;
-    }  
+    }
     var waitList= [];
     var toWhat;
     var trick = game.currentHand().currentTrick();
-    
+
     if (trick.cardsPlayed.length === 4) {
       var allConfirmed = true;
-           
+
       for (var p in game.players) {
         if (game.players[p].confirmed === false) {
           allConfirmed = false;
@@ -338,9 +339,9 @@ class Player {
       toWhat = "play";
     }
 
-    
 
-    
+
+
     if (allConfirmed){
       for (var p in game.players) {
         game.players[p].confirmed = false;
@@ -350,13 +351,13 @@ class Player {
         hand.startTrick();
       } else {
         hand.finish();
-      }  
+      }
     } else {
       this.waitingFor(waitList, toWhat);
       game.update += 1;
     }
   }
-  
+
   findPlayerInPlayOrder(trick) {
     for (var i = 0; i < trick.playOrder.length; i ++) {
       if (trick.playOrder[i] === this) {
@@ -364,7 +365,7 @@ class Player {
       }
     }
   }
-  
+
   illegalCardReset(trick){
     trick.hand.game.update += 1;
     this.attempts += 1;
@@ -373,11 +374,11 @@ class Player {
       trick.nextPlayer(i);
     }, 0);
   }
-  
-  
+
+
   isLegalCard(trick, handCards, card) {
     if (trick.cardsPlayed.length === 0) {
-    // Player is leading trick:  
+    // Player is leading trick:
       if (trick.spadesBroken) {
       // Spades is broken:
         return true;
@@ -400,7 +401,7 @@ class Player {
           }
           return true;
         }
-      }     
+      }
     } else {
     // Player is not leading tick:
       var ledSuit = trick.cardsPlayed[0].suit;
@@ -408,7 +409,7 @@ class Player {
       if (card.suit === ledSuit) {
         return true;
       } else {
-      // Card suit does not match ledSuit:  
+      // Card suit does not match ledSuit:
         var hasLedSuit = false;
         for (var c in handCards) {
           if (handCards[c].suit === ledSuit) {
@@ -420,7 +421,7 @@ class Player {
         // But player has led suit in hand
         // Illegal card
           return false;
-        } else { 
+        } else {
         // Player does not have led suit in hand
           if (handCards.length < 13) {
           // Any card is legal on 2nd to 13th tricks if
@@ -445,29 +446,29 @@ class Player {
               return true;
             }
           }
-        } 
+        }
       }
     }
     for (var i = 0; i > 100; i++) {
       console.log("ERROR: DID NOT RETURN");
     }
   }
-  
+
   waitingFor(waitList, toWhat) {
     var waitNames = "";
     for (var n in waitList) {
       waitNames += " " + waitList[n] + ",";
     }
     waitNames = waitNames.slice(0, -1);
-    
+
     this.setStatus("waitingForConfirms", {
       "question": "Waiting for" + waitNames + " to " + toWhat + ".",
       "type": "options",
       "options": []
-    }); 
+    });
   }
-  
-  
+
+
 }
 
 module.exports = Player;
